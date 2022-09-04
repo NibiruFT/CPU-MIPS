63d690879880699e433abddf27cdfd3116e4567b
Felipe Targino da Silva <93540956+NibiruFT@users.noreply.github.com>
2022-09-04 10:49:51 -0300

    Add files via upload

---
+++ Sprint 7/respostas/Adder_Branch.v
@@ -0,0 +1,4 @@
+module AdderBranch(input [7:0] Ent0, Ent1,
+					output[7:0] Sai);			
+	assign Sai = Ent0 + Ent1;
+endmodule
---
+++ Sprint 7/respostas/MuxJump.v
@@ -0,0 +1,10 @@
+module MuxJump(input [7:0]Ent1, Ent0, input S, 
+					  output reg[7:0] Sai);	
+	always @*begin
+		if(S == 0) begin
+			Sai = Ent0;
+		end	
+		else	
+			Sai = Ent1; 
+	end
+endmodule
